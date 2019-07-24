af6d834219a28dd34c5e83c5768493d6f5795fdc
mezyapps <mezyapps@gmail.com>
2019-07-24 16:51:08 +0530

    NJ commit 3 24-07-2019

---
+++ app/src/main/java/com/mezyapps/bizprotect/view/fragment/MyBlackListedCustomerFragment.java
@@ -0,0 +1,175 @@
+package com.mezyapps.bizprotect.view.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.mezyapps.bizprotect.R;
+import com.mezyapps.bizprotect.apicommon.ApiClient;
+import com.mezyapps.bizprotect.apicommon.ApiInterface;
+import com.mezyapps.bizprotect.model.ClientProfileModel;
+import com.mezyapps.bizprotect.model.MyBlackListedCustomerModel;
+import com.mezyapps.bizprotect.model.SuccessModel;
+import com.mezyapps.bizprotect.utils.NetworkUtils;
+import com.mezyapps.bizprotect.utils.SharedLoginUtils;
+import com.mezyapps.bizprotect.utils.ShowProgressDialog;
+import com.mezyapps.bizprotect.view.activity.AddCustomerActivity;
+import com.mezyapps.bizprotect.view.adpater.MyBlackListedCustomerListAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class MyBlackListedCustomerFragment extends Fragment {
+
+    private Context mContext;
+    private RecyclerView recyclerView_ourCustomer;
+    private FloatingActionButton fab_add_customer;
+    private EditText edt_search;
+    private MyBlackListedCustomerListAdapter myBlackListedCustomerListAdapter;
+    private ArrayList<MyBlackListedCustomerModel> myBlackListedCustomerModelArrayList = new ArrayList<>();
+    private SwipeRefreshLayout swipeRefresh_our_customer;
+    public static ApiInterface apiInterface;
+    private ArrayList<ClientProfileModel> clientProfileModelArrayList=new ArrayList<>();
+    private String client_id;
+    private ShowProgressDialog showProgressDialog;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_my_blacklisted_customer, container, false);
+        mContext = getContext();
+
+        find_View_IdS(view);
+        events();
+
+        return view;
+    }
+
+    private void find_View_IdS(View view) {
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        recyclerView_ourCustomer = view.findViewById(R.id.recyclerView_ourCustomer);
+        fab_add_customer = view.findViewById(R.id.fab_add_customer);
+        edt_search = view.findViewById(R.id.edt_search);
+        swipeRefresh_our_customer = view.findViewById(R.id.swipeRefresh_our_customer);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
+        recyclerView_ourCustomer.setLayoutManager(linearLayoutManager);
+        recyclerView_ourCustomer.hasFixedSize();
+
+        clientProfileModelArrayList= SharedLoginUtils.getUserDetails(mContext);
+        client_id=clientProfileModelArrayList.get(0).getClient_id();
+
+        showProgressDialog=new ShowProgressDialog(mContext);
+
+
+        if (NetworkUtils.isNetworkAvailable(mContext)) {
+            listOurCustomer();
+        }
+        else {
+            NetworkUtils.isNetworkNotAvailable(mContext);
+        }
+
+    }
+
+    private void events() {
+
+
+        swipeRefresh_our_customer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                if (NetworkUtils.isNetworkAvailable(mContext)) {
+                    listOurCustomer();
+                }
+                else {
+                    NetworkUtils.isNetworkNotAvailable(mContext);
+                }
+                swipeRefresh_our_customer.setRefreshing(false);
+            }
+        });
+
+        fab_add_customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(mContext, AddCustomerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void listOurCustomer() {
+        showProgressDialog.showDialog();
+
+        Call<SuccessModel> call = apiInterface.myBlackListed(client_id);
+        call.enqueue(new Callback<SuccessModel>() {
+            @Override
+            public void onResponse(Call<SuccessModel> call, Response<SuccessModel> response) {
+                showProgressDialog.dismissDialog();
+                String str_response = new Gson().toJson(response.body());
+                Log.d("Response >>", str_response);
+
+                try {
+                    if (response.isSuccessful()) {
+                        SuccessModel successModule = response.body();
+
+                        String message = null, code = null;
+                        if (successModule != null) {
+                            message = successModule.getMessage();
+                            code = successModule.getCode();
+                            if (code.equalsIgnoreCase("1")) {
+                                myBlackListedCustomerModelArrayList=successModule.getMyBlackListedCustomerModelArrayList();
+                                Collections.reverse(myBlackListedCustomerModelArrayList);
+                                if(myBlackListedCustomerModelArrayList.size()!=0) {
+                                    myBlackListedCustomerListAdapter = new MyBlackListedCustomerListAdapter(mContext, myBlackListedCustomerModelArrayList);
+                                    recyclerView_ourCustomer.setAdapter(myBlackListedCustomerListAdapter);
+                                    myBlackListedCustomerListAdapter.notifyDataSetChanged();
+                                }
+                                else
+                                {
+
+                                }
+                            } else {
+                                Toast.makeText(mContext, "No Any Customer", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        } else {
+                            Toast.makeText(mContext, "Response Null", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<SuccessModel> call, Throwable t) {
+                showProgressDialog.dismissDialog();
+            }
+        });
+    }
+
+
+}
