89dfa2bc740a9347328c701555e93dc182d7cc51
mezyapps <mezyapps@gmail.com>
2019-08-10 18:34:06 +0530

    NJ Commit 09-08-2019 (1)

--- app/src/main/java/com/mezyapps/bizprotect/view/fragment/AddBillFragment.java
+++ app/src/main/java/com/mezyapps/bizprotect/view/fragment/AddBillFragment.java
@@ -1,5 +1,6 @@
 package com.mezyapps.bizprotect.view.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,11 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.mezyapps.bizprotect.R;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -20,10 +23,12 @@ public class AddBillFragment extends Fragment {
     private Context mContext;
     private EditText edit_date;
     private String date;
+    private DatePickerDialog datePickerDialog;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_add_bill, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_bill, container, false);
 
         find_View_IDs(view);
         events();
@@ -32,13 +37,40 @@ public class AddBillFragment extends Fragment {
     }
 
     private void find_View_IDs(View view) {
-        edit_date=view.findViewById(R.id.edit_date);
-        date= new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        edit_date = view.findViewById(R.id.edit_date);
+        date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
         edit_date.setText(date);
     }
 
     private void events() {
+        edit_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callCalendarPicker();
+            }
+        });
+    }
 
+    private void callCalendarPicker() {
+
+        Calendar calendar = Calendar.getInstance();
+        int mYear = calendar.get(Calendar.YEAR);
+        int mMonth = calendar.get(Calendar.MONTH);
+        int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        datePickerDialog = new DatePickerDialog(mContext,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        date = String.format("%s-%s-%s", year, monthOfYear, dayOfMonth);
+                        edit_date.setText(date);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 
 }
